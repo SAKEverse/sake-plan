08feeedc7deb0969484de4e5fe0ec4cd6d761094
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-07-14 17:29:23 -0400

    add user input and button callbacks

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 
 ### ---------------------------- Imports ---------------------------- ###
+import os
 import dash, dash_table
 import dash_core_components as dcc
-import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import dash_daq as daq
 from dash.exceptions import PreventUpdate
+
+### ------------- USER -------------- ###
+from adi_read import AdiParse
+
 ### ----------------------------------------------------------------- ###
 
 
@@ -14,16 +19,52 @@ app = dash.Dash(__name__ , external_stylesheets=['https://codepen.io/chriddyp/pe
 
 app.layout = html.Div(id = 'page-content', children=[
 
+    # add data from path
+    html.Div(id='data_path_main_div', 
+        children = [ 
+            html.Div(id='data_path_main_text', children = ['Data Path']),
+            dcc.Input(id='data_path_input', type='text', placeholder='C:/'),
+            html.Div(id='out_all_types'),
+        ]),
 
     # Generate button
-    html.Div(   html.Button('Generate', id='generate-button', n_clicks=0,
+    html.Div( children =[
+        html.Button('Generate', id='generate_button', n_clicks=0,
                 style = {'color': 'white','background-color': '#4CAF50'}),
-                style = {'display' : 'table-cell', 'font-size': '13px'} 
-                ),
+            ]),
 
 
-
+# end of layout
 ])
+
+
+
+### ------------------- Define callbacks ------------------- ###
+
+
+# Retrieve path and plot tree diagram
+@app.callback(
+    Output('out_all_types', 'children'),
+    [Input('generate_button', 'n_clicks')],
+    [State('data_path_input', 'value')],
+)
+def update_output(n_clicks, folder_path):
+
+    if folder_path is None:
+        folder_path = 'empty'
+    try:
+        # initiate object        
+        adi_read = AdiParse(folder_path)
+
+        # get grouped dataframe
+        df, unique_groups = adi_read.get_unique_conditions()
+
+        # export to csv
+        df.to_csv('sankey_data.csv')
+
+    except Exception as err:
+        return 'Got ' + str(folder_path) + ': ' + str(err)
+    return str(folder_path)
 
 
 
