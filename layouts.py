0678ffd545b4eb4746ed51ac5e9486c3a7de7154
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-07-16 16:34:21 -0400

    add table to layout

--- layouts.py
+++ layouts.py
@@ -1,69 +1,52 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import dash_daq as daq
 
 # Always present
-layout_common =  html.Div(id = 'layout_channel', children=[
+layout1 =  html.Div(id = 'layout_channel', children=[
 
-    #1- Separation type dropdown
-    html.Div( id='main_dropdown_div', children = [
-        dcc.Dropdown(
-            id='main_dropdown', className='dcc_dropdown',
-            options=[
-                {'label': 'Separate by channel names', 'value': 'channel_name'},
-                {'label': 'Separate by file name', 'value':  'file_name'},
-            ], value='channel_name', clearable=False),
-    ]),
+    # 1- generate button + field div
+    html.Div(id='generate_plus_field_div', children=[
 
-    # 2- Input data path
-    html.Div(id='data_path_main_div', 
-        children = [ 
-            # html.Div(id='data_path_main_text', children = ['Data Path']),
-            dcc.Input(id='data_path_input', type='text', placeholder='Path-to-data'),
-            html.Div(id='out_all_types'),
-    ]),
+        html.Div( id='generate_div', children=[
+            html.Button('Generate', id='generate_button', n_clicks=0,   
+            ),]),
 
-    # 3- Generate plot and table button
-    html.Div( id='generate_div', children =[
-        html.Button('Generate', id='generate_button', n_clicks=0,   
-        ),
-    ]),
-
-]) # end of this layout
-
-
-# If files are grouped by channel name
-layout_file =  html.Div(id = 'layout_file_name', children=[
-
-    # 1- Unique Channels
-    html.Div(id='unique_channels_div', 
-        children = [ 
-
-        # unique channel number     
-        daq.NumericInput(
-            id='unique_channel_number',
-            value=3
-        ),
-        html.Div(id = 'unique_channel_label_container', children =[
-        html.Div(id= 'unique_channel_label', children = 'Unique Channels'),
+        html.Div(id='data_path_main_div', 
+            children = [ 
+                dcc.Input(id='data_path_input', type='text', placeholder='Path-to-data'),
+                html.Div(id='out_all_types'),
         ]),
-        ]),
+    ]),
 
-    # 3- Unique channel field
-    html.Div(id='unique_channels_inputs'),
+    # 2- User table
+     html.Div( id='add_row_button_div', children=[
+            html.Button('Add row', id='add_row_button', n_clicks=0,   
+            ),]),
+
+    # create datatable
+    html.Div(id='user_table_div', children=[
+
+        dash_table.DataTable(id = 'user_table',
+                    editable = True,
+                    style_cell={
+                            'color': 'black',
+                            'textAlign': 'center',
+                            'font-family':'arial',
+                            'font-size': '100%',
+                            },
+                    style_header={
+                        'fontWeight': 'bold',
+                        'color': 'black',
+                        'textAlign': 'center',
+                        'backgroundColor': 'rgb(230, 230, 230)',
+                            },
+                    # style_data={
+                    #     'width': '125px', 'minWidth': '125px', 'maxWidth': '125px'}
+            )
+    ]),
+
+])
 
 
-    # 4- Unique channel field
-    html.Div(id='channel_inputs'),
-
-]) # end of this layout
-
-
-# If files are grouped by file name
-layout_channel =  html.Div(id = 'layout_file_name', children=[
-
-    # 1- Example
-    html.Div(id='tree_plot_div'
-        ),
-   
-]) # end of this layout
