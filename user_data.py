dc4df949a8ac4423b4c964f580d8c86d6882f986
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-09-28 12:29:44 -0400

    Create user_data.py

---
+++ user_data.py
@@ -0,0 +1,34 @@
+import io, base64
+import pandas as pd
+
+# define path to load table
+user_table_path = 'example_data/default_table_data.csv'
+
+# load table
+original_user_data = pd.read_csv(user_table_path)
+
+# get original columns
+user_data_columns = original_user_data.columns
+
+def upload_csv(upload_contents:str):
+    """
+    PARAMETERS
+    ---------
+    upload_contents: str, contents from dcc upload object
+    
+    OUTPUT
+    ----------
+    df: pd.DataFrame, with user data
+
+    """
+
+    # parse user csv as df
+    _, content_string = upload_contents.split(',')
+    decoded = base64.b64decode(content_string)
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+    # if columns do not match load original user data
+    if set(df.columns) != set(user_data_columns):
+        df = original_user_data
+
+    return df
