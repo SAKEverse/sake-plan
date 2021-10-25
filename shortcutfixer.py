a0fed8e5ca4c9dd39f7f020f2e13e409f1e75974
Grant Weiss <61599897+researchgrant@users.noreply.github.com>
2021-10-25 12:57:29 -0400

    update shortcutfix

--- shortcutfixer.py
+++ shortcutfixer.py
@@ -7,18 +7,24 @@ Created on Mon Oct 25 10:32:37 2021
 
 import os
 
+doit=True
 conda_path=os.path.join(r"C:\Users",os.getlogin())
 if 'miniconda3' in os.listdir(conda_path):
     conda_path=os.path.join(conda_path,r'miniconda3\Scripts','activate.bat')
 elif 'anaconda3' in os.listdir(conda_path):
     conda_path=os.path.join(conda_path,r'anaconda3\Scripts','activate.bat')
 else:
-    raise Exception("Conda not found, recommend installing miniconda in user directory")
+    print("Conda not found, recommend installing miniconda in user directory")
+    input()
+    doit=False
 
-file = open("sake.cmd","w")
-lines=[]
-lines.append( r"call "+conda_path)
-lines.append( '\ncd '+os.getcwd() )
-lines.append( '\npipenv run python sake.py')
-file.writelines(lines)
-file.close()
+if doit == True:
+    file = open("sake.cmd","w")
+    lines=[]
+    lines.append( r"call "+conda_path)
+    lines.append( '\ncd '+os.getcwd() )
+    lines.append( '\npipenv run python sake.py')
+    file.writelines(lines)
+    file.close()
+    print("Shortcut Fixed!")
+    input()
