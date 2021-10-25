cdcc2dad2deba006351b4e51af8023daed89abc8
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-10-25 15:39:27 -0400

    add conditionals

--- sake.cmd
+++ sake.cmd
@@ -1,11 +1,27 @@
-call %USERPROFILE%\miniconda3\Scripts\activate.bat
-call %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
+:: Check if Miniconda is present
+IF EXIST %USERPROFILE%\miniconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\miniconda3
+)
+
+:: Check if Anaconda is present
+IF EXIST %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\AppData\Local\Continuum\anaconda3
+)
+
+:: enter conda
+call %conda_file%\Scripts\activate.bat
+
+:: Navigate sake-plan directory
 cd %USERPROFILE%\Documents\GitHub\sake-plan
-pip install pipenv
-IF EXIST Pipfile.lock (
-    echo "Environment already installed"
-) ELSE (
+
+:: Check if pipenv has been installed
+IF NOT EXIST %conda_file%\Lib\site-packages\pipenv\__main__.py (
+    pip install pipenv
+)
+
+IF NOT EXIST Pipfile.lock (
     pipenv install
 )
+
+:: Launch app
 pipenv run python sake.py
-SLEEP 100
