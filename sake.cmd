ac5e492e9d55e9be45c102d5b9631e2d1ffa4548
Grant Weiss <61599897+researchgrant@users.noreply.github.com>
2021-10-25 14:37:44 -0400

    Update sake.cmd

--- sake.cmd
+++ sake.cmd
@@ -1,5 +1,11 @@
 call %USERPROFILE%\miniconda3\Scripts\activate.bat
 call %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
 cd %USERPROFILE%\Documents\GitHub\sake-plan
+pip install pipenv
+IF EXIST Pipfile.lock (
+    echo "Environment already installed"
+) ELSE (
+    pipenv install
+)
 pipenv run python sake.py
 SLEEP 100
