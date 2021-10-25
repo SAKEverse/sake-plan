da2867e416a52251d30f19338ad45763d8b78b56
researchgrant <61599897+researchgrant@users.noreply.github.com>
2021-10-25 14:03:38 -0400

    Update sake.cmd

--- sake.cmd
+++ sake.cmd
@@ -1,3 +1,5 @@
 call %USERPROFILE%\miniconda3\Scripts\activate.bat
+call %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
 cd %USERPROFILE%\Documents\GitHub\sake-plan
 pipenv run python sake.py
+SLEEP 100
